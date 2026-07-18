3e6c2c01cc5180aa59e2cb0fb38d513354b15283
Kenny200 <58740689+Kenny200@users.noreply.github.com>
2026-07-18 12:15:20 -0400

    Refactor Weasel program with mutation and fitness functions
    Refactor weasel program to improve functionality and structure. Added mutation and fitness functions for offspring generation.

--- weaeal.py
+++ weaeal.py
@@ -1,20 +1,53 @@
 #My Weasel Program
 import random
 import string
+
 target_phrase = "METHINKS IT IS LIKE A WEASEL"
-
 letters = string.ascii_uppercase + ' '
+num_offspring = 100
+probability = 5
 
-def generateword(length):
-    return ''.join(random.choice(letters) for _ in range(length))
+def generate_word():
+    return ''.join(random.choice(letters) for _ in range(28))
 
-primordal = generate_word(28)
+adam = generate_word()
 
-def create_offspring(ancestor):
-    newlist = list(ancestor)
+def generate_char():
+    return random.choice(string.ascii_uppercase + ' ')
+
+def mutate(string):
+    charlist = []
+    for char in string:
+        if random.randint(1,probability) == 1:
+            charlist.append(char)
+        else:
+            charlist.append(generate_char())
+    return ''.join(charlist)
+
+def create_offspring(parent):
+    offspring_list = []
+    for i in range(num_offspring):
+        offspring_list.append(mutate(parent))
+    return offspring_list
+
+def fitness(child):
+    child_char = list(child)
+    target_char = list(target_phrase)
+    fitness_score = 0
+    for i in len(child_char):
+        if i == target_char[i]:
+            fitness_score += 1
+    return fitness_score
+
+sons_of_adam = create_offspring(adam)
+
+while cain != adam:
+    fitness_scores = []
+    for string in sons_of_adam:
+        fitness(string)
+        fitness_scores.append(string)
+    print(fitness_scores)
+
     
-
-
-#while ancestor != target_phrase:
-    
-print(primordal)
+print(adam)
+#print(cain)
